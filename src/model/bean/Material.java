22b6e69777e290741f22285aba3b721e1149297f
Felipemv <felipe.martinsvitor@gmail.com>
2017-12-28 15:53:21 -0200

    CRUD de produtos com banco de dados

---
+++ src/model/bean/Material.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bean;
+
+/**
+ *
+ * @author Usuário
+ */
+public class Material {
+    private int id;
+    private int id_produto;
+    private long cod;
+    private String nome;
+    private int quant;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_produto() {
+        return id_produto;
+    }
+
+    public void setId_produto(int id_produto) {
+        this.id_produto = id_produto;
+    }
+
+    public long getCod() {
+        return cod;
+    }
+
+    public void setCod(long cod) {
+        this.cod = cod;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuant() {
+        return quant;
+    }
+
+    public void setQuant(int quant) {
+        this.quant = quant;
+    }
+    
+    
+    
+}
---
+++ src/model/dao/ProdutoDAO.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.dao;
+
+import connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import model.bean.Produto;
+
+/**
+ *
+ * @author Usuário
+ */
+public class ProdutoDAO {
+    
+    private static final String COL_ID = "id";
+    private static final String COL_NOME = "nome";
+    
+    public void create(Produto p) {
+        Connection connection = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = connection.prepareStatement("INSERT INTO produtos(nome) VALUES(?)");
+            stmt.setString(1, p.getNome());
+            
+            stmt.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Salvo com sucesso!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao salvar: "+ex);
+        }finally{
+            ConnectionFactory.closeConnection(connection, stmt);
+        }
+    }
+    
+    public List<Produto> read(){
+        Connection connection = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        List<Produto> produtos = new ArrayList<>();
+        
+        try {
+            stmt = connection.prepareStatement("SELECT * FROM produtos");                        
+            rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                Produto p = new Produto();
+                
+                p.setId(rs.getInt(COL_ID));
+                p.setNome(rs.getString(COL_NOME));
+                
+                produtos.add(p);
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao carregar: "+ex);
+        }finally{
+            ConnectionFactory.closeConnection(connection, stmt, rs);
+        }        
+        return produtos;
+    }
+    
+    public void update(Produto p) {
+        Connection connection = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = connection.prepareStatement("UPDATE produtos SET nome = ? WHERE id = ?");            
+            stmt.setString(1, p.getNome());
+            stmt.setInt(2, p.getId());
+            
+            stmt.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Atualizado com sucesso!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao atualizar: "+ex);
+        }finally{
+            ConnectionFactory.closeConnection(connection, stmt);
+        }
+    }
+    
+    public void delete(Produto p) {
+        Connection connection = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = connection.prepareStatement("DELETE FROM produtos WHERE id = ?");   
+            stmt.setInt(1, p.getId());
+            
+            stmt.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Excluído com sucesso!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao excluir: "+ex);
+        }finally{
+            ConnectionFactory.closeConnection(connection, stmt);
+        }
+    }
+}
